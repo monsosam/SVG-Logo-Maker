44beadf9bc445d4d5a9f218fd3ecec775f9aabb0
monsosam <monsonsamuel5@gmail.com>
2024-02-23 21:35:41 -0700

    Modified to run test

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-import fs from "fs";
+const inquirer = require('inquirer');
+const fs = require('fs');
 import { Triangle, Square, Circle } from "./lib/shapes.js";
 
-function writeToFile(fileName, answers) {
+const allowedColors = [
+  "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond",
+  "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue",
+  "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki",
+  "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue",
+  "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick",
+  "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow",
+  "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon",
+  "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink", "LightSalmon",
+  "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen",
+  "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue",
+  "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin",
+  "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod",
+  "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue",
+  "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen",
+  "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue",
+  "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"
+];
+
+async function writeToFile(fileName, answers) {
   let svgStr = "";
 
   svgStr =
@@ -24,17 +44,17 @@ function writeToFile(fileName, answers) {
   svgStr += "</g>";
   svgStr += "</svg>";
 
-  fs.writeFile(fileName, svgStr, (err) => {
-    err ? console.log(err) : console.log("Generated logo.svg");
-  });
+  try {
+    await fs.writeFile(fileName, svgStr);
+    console.log("Generated logo.svg");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function promptUser() {
-  const inquirerModule = await import("inquirer");
-  const inquirer = inquirerModule.default;
 
-  inquirer
-    .prompt([
+  inquirer.prompt([
       {
         type: "input",
         message:
@@ -61,6 +81,15 @@ async function promptUser() {
         message:
           "Choose shapes color (Enter color keyword OR a hexadecimal number)",
         name: "shapeBackgroundColor",
+        validate: function (input) {
+          const color = input.toLowerCase(); //Convert user input to lowercase
+          const isValidHexColor = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input);
+          if (isValidHexColor || allowedColors.map(c => c.toLowerCase()).includes(color)) {
+            return true; // Input is a valid color
+          } else {
+            return "Please enter a valid color (color name or hexadecimal value).";
+          }
+        }
       },
     ])
     .then((answers) => {
